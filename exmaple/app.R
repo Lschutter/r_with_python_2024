cee244576cc27459ab2e4e81c82a3d9f04464560
Lschutter <schutter.8@buckeyemail.osu.edu>
2024-07-17 20:41:36 -0400

    Update app.R

--- exmaple/app.R
+++ exmaple/app.R
@@ -1,51 +1,113 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("BSGP 2024 LM Dashboard"),
     
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            # Input: Select a file ----
+            fileInput("file1", "Choose CSV File",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv")),
             
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Input: Checkbox if file has header ----
+            checkboxInput("header", "Header", TRUE),
+            
+            # Input: Select separator ----
+            radioButtons("sep", "Separator",
+                         choices = c(Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"),
+                         selected = ","),
+            
+            # Input: Select quotes ----
+            radioButtons("quote", "Quote",
+                         choices = c(None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"),
+                         selected = '"'),
+            
+            # Horizontal line ----
+            tags$hr(),
+            
+            # Input: Select number of rows to display ----
+            radioButtons("disp", "Display",
+                         choices = c(Head = "head",
+                                     All = "all"),
+                         selected = "head"),
+            tags$hr(),
+            actionButton("go", "Plot Linear Model")
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
-            tableOutput("contents"),
-            plotOutput("distPlot")
-            
+           plotOutput("origPlot"),
+           plotOutput("lmPlot"),
+           tableOutput("contents"),
+           verbatimTextOutput("modelSummary"),
+           verbatimTextOutput("corrCoeff")
         )
     )
-
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    lmdata <- reactiveValues()
+    
+    dataInput <- reactive({
+        req(input$file1)
+        
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+        return(df)
+    })
+    
+    observeEvent(input$go, {
+        update_lm()
+    })
+    
+    update_lm <- function(){
+        lmdata$model <- lm(y ~ x, data = dataInput())
+    }
+    
+    output$origPlot <- renderPlot({
+        plot(dataInput()$x, dataInput()$y, xlab = "X", ylab = "Y", main = "Original Data")
+    })
+    
+    output$lmPlot <- renderPlot({
+        plot(dataInput()$x, dataInput()$y, xlab = "X", ylab = "Y", main = "Linear Model")
+        abline(lmdata$model, col = "red")
+    })
+    
+    output$contents <- renderTable({
+        if(input$disp == "head") {
+            return(head(dataInput()))
+        } else {
+            return(dataInput())
+        }
+    })
+    
+    output$modelSummary <- renderText({
+        req(lmdata$model)
+        slope <- coef(lmdata$model)[2]
+        intercept <- coef(lmdata$model)[1]
+        paste("Slope:", round(slope, 3), "Intercept:", round(intercept, 3))
+    })
+    
+    output$corrCoeff <- renderText({
+        req(lmdata$model)
+        corr_coeff <- cor(dataInput()$x, dataInput()$y)
+        paste("Correlation Coefficient:", round(corr_coeff, 3))
     })
 }
 
